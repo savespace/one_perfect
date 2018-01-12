a21ee200f1275ec2767c19f78fde1932de44456c
Krzysztof Zielonka <savespace@outlook.com>
2018-01-12 18:58:46 +0100

    location

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function initMap() {
 	var location = {lat: 52.644945, lng: 16.087175};
 	var map= new google.maps.Map(document.getElementById("map"), {
-		zoom: 4, 
+		zoom: 16, 
 		center: location
 	});
 	var marker = new google.maps.Marker({
